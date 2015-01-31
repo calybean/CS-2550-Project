7b41a887a1b5dd421227ef1205b82725f3f5bd60
youravgjoe <droidboss47@gmail.com>
2015-01-30 21:03:20 -0700

    Finished Javascript Changes
    1/30/15

--- Assignment 02/game.js
+++ Assignment 02/game.js
@@ -2,10 +2,11 @@
 
 function page_init() {
   document.getElementById('startGame').onclick = button_click;
+  document.getElementById('player1Input').focus();
+
 }
 
 function generate_table() {
-  //I know this isn't the prettiest way to do this, but it was the only way I could get it to work.
   document.getElementById('table').innerHTML = '<table class="black" align="center" border="4"><tr align="center"><td class="black"><img src="red_plain.png"></td><td class="red"></td><td class="black"><img src="red_plain.png"></td><td class="red"></td><td class="black"><img src="red_plain.png"></td><td class="red"></td><td class="black"><img src="red_plain.png"></td><td class="red"></td></tr><tr align="center"><td class="red"></td><td class="black"><img src="red_plain.png"></td><td class="red"></td><td class="black"><img src="red_plain.png"></td><td class="red"></td><td class="black"><img src="red_plain.png"></td><td class="red"></td><td class="black"><img src="red_plain.png"></td></tr><tr align="center"><td class="black"><img src="red_plain.png"></td><td class="red"></td><td class="black"><img src="red_plain.png"></td><td class="red"></td><td class="black"><img src="red_plain.png"></td><td class="red"></td><td class="black"><img src="red_plain.png"></td><td class="red"></td></tr><tr align="center"><td class="red"></td><td class="black"></td><td class="red"></td><td class="black"></td><td class="red"></td><td class="black"></td><td class="red"></td><td class="black"></td></tr><tr align="center"><td class="black"></td><td class="red"></td><td class="black"></td><td class="red"></td><td class="black"></td><td class="red"></td><td class="black"></td><td class="red"></td></tr><tr align="center"><td class="red"></td><td class="black"><img src="black_plain.png"></td><td class="red"></td><td class="black"><img src="black_plain.png"></td><td class="red"></td><td class="black"><img src="black_plain.png"></td><td class="red"></td><td class="black"><img src="black_plain.png"></td></tr><tr align="center"><td class="black"><img src="black_plain.png"></td><td class="red"></td><td class="black"><img src="black_plain.png"></td><td class="red"></td><td class="black"><img src="black_plain.png"></td><td class="red"></td><td class="black"><img src="black_plain.png"></td><td class="red"></td></tr><tr align="center"><td class="red"></td><td class="black"><img src="black_plain.png"></td><td class="red"></td><td class="black"><img src="black_plain.png"></td><td class="red"></td><td class="black"><img src="black_plain.png"></td><td class="red"></td><td class="black"><img src="black_plain.png"></td></tr></table>';
 }
 
@@ -18,8 +19,82 @@ function button_click() {
   player2.value = "";
   player1.focus();
 
-  generate_table();
+  // Change number of rows and columns here:
+  var rows = 8;
+  var columns = 8;
+
+  //generate the table
+  generate_dynamic_table(rows, columns);
   return false;
+}
+
+function generate_dynamic_table(tableRows, tableColumns) {
+  var rows = tableRows;
+  var columns = tableColumns;
+  var endRed = 3;
+  var startBlack = rows - 3;
+  var table = document.getElementById('table');
+  var tableString = "";
+
+  // start table:
+  tableString += '<table class="black" align="center" border="4">';
+
+  for (var i = 0; i < rows; i++) {
+    tableString += '<tr align="center">';
+    for (var j = 0; j < columns; j++) {
+      // first 3 rows:
+      if (i < endRed) {
+        if (((i % 2) === 0) && ((j % 2) === 0)) {
+          //even even
+          tableString += '<td class="black"><img src="red_plain.png"></td>';
+        } else if ((((i % 2) === 0) && (j % 2) !== 0)) {
+          //even odd
+          tableString += '<td class="red"></td>';
+        } else if (((i % 2) !== 0) && ((j % 2) === 0)) {
+          //odd even
+          tableString += '<td class="red"></td>';
+        } else {
+          //odd odd
+          tableString += '<td class="black"><img src="red_plain.png"></td>';
+        }
+        // middle rows:
+      } else if ((i > endRed - 1) && (i < startBlack)) {
+        if (((i % 2) === 0) && ((j % 2) === 0)) {
+          //even even
+          tableString += '<td class="black"></td>';
+        } else if (((i % 2) === 0) && ((j % 2) !== 0)) {
+          //even odd
+          tableString += '<td class="red"></td>';
+        } else if (((i % 2) !== 0) && ((j % 2) === 0)) {
+          //odd even
+          tableString += '<td class="red"></td>';
+        } else {
+          //odd odd
+          tableString += '<td class="black"></td>';
+        }
+        // last 3 rows:
+      } else {
+        if (((i % 2) === 0) && ((j % 2) === 0)) {
+          //even even
+          tableString += '<td class="black"><img src="black_plain.png"></td>';
+        } else if ((((i % 2) === 0) && (j % 2) !== 0)) {
+          //even odd
+          tableString += '<td class="red"></td>';
+        } else if (((i % 2) !== 0) && ((j % 2) === 0)) {
+          //odd even
+          tableString += '<td class="red"></td>';
+        } else {
+          //odd odd
+          tableString += '<td class="black"><img src="black_plain.png"></td>';
+        }
+      }
+    }
+  }
+  // end table:
+  tableString += '</table>';
+
+  // after concatenating the entire HTML string, give it to the innerHTML of the table.
+  table.innerHTML = tableString;
 }
 
 if (document && document.getElementById) {
